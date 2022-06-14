60d425a07f79a6e8991468e6efbd60f200dc6f55
Blake McCarty <blake_mccarty@yahoo.com>
2022-06-14 14:22:15 -0500

    utilize process.env in getforcast function

--- src/scripts/views/C_F_btn_views.js
+++ src/scripts/views/C_F_btn_views.js
@@ -1,10 +1,4 @@
 //checks the current status of the DOM's measurement button. If toggle is true it changes C - F;
-
-import {
-  getTempPreference,
-  setTempPreference,
-} from "../models/manage_stroage_model";
-
 //if it is false it returns the current value of the button. C or F;
 export const toggleCelcFaren = (toggle) => {
   const celc = $("#C-F-btn").hasClass("C");
@@ -12,14 +6,13 @@ export const toggleCelcFaren = (toggle) => {
   const btn = $("#C-F-btn");
 
   if (!toggle) {
-    const mesType = getTempPreference();
-    return mesType;
+    if (celc) return "C";
+    if (far) return "F";
   }
 
   //if toggle is true, C and F are toggled below and the appropriate value is returned.
   btn.toggleClass("C");
   btn.toggleClass("F");
-  setTempPreference(btn.attr("class"));
   if (celc) return "F";
   if (far) return "C";
 };
