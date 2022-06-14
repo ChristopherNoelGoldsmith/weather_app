c506f114cf484c06061214dce7f469fb3bab1c3c
Blake McCarty <blake_mccarty@yahoo.com>
2022-06-14 14:18:38 -0500

    utilize local storage while toggling button

--- src/scripts/views/C_F_btn_views.js
+++ src/scripts/views/C_F_btn_views.js
@@ -1,18 +1,25 @@
 //checks the current status of the DOM's measurement button. If toggle is true it changes C - F;
+
+import {
+  getTempPreference,
+  setTempPreference,
+} from "../models/manage_stroage_model";
+
 //if it is false it returns the current value of the button. C or F;
 export const toggleCelcFaren = (toggle) => {
-  const celc = $('#C-F-btn').hasClass('C');
-  const far = $('#C-F-btn').hasClass('F');
-  const btn = $('#C-F-btn');
+  const celc = $("#C-F-btn").hasClass("C");
+  const far = $("#C-F-btn").hasClass("F");
+  const btn = $("#C-F-btn");
 
   if (!toggle) {
-    if (celc) return 'C';
-    if (far) return 'F';
+    const mesType = getTempPreference();
+    return mesType;
   }
 
   //if toggle is true, C and F are toggled below and the appropriate value is returned.
-  btn.toggleClass('C');
-  btn.toggleClass('F');
-  if (celc) return 'F';
-  if (far) return 'C';
+  btn.toggleClass("C");
+  btn.toggleClass("F");
+  setTempPreference(btn.attr("class"));
+  if (celc) return "F";
+  if (far) return "C";
 };
